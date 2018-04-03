26e54d159b891e90f713c848c942e5dd2517d6aa
danbatis <danielbatista.contato@gmail.com>
2018-04-03 00:59:43 -0700

    linking attack animations

--- EpidemicVectorsUnity/Assets/danbatis/scripts/ProxyRobotControl.cs
+++ EpidemicVectorsUnity/Assets/danbatis/scripts/ProxyRobotControl.cs
@@ -9,6 +9,8 @@ public class ProxyRobotControl : MonoBehaviour {
 	public float maxVertVelUp = 50f;
 	public float maxVertVelDown = 80f;
 	public float inertiaFactor = 2.0f;
+	float startJumpTime;
+	public float jumpTime = 0.5f;
 
 	public float thrustAcel = 10.0f;
 	public float frictionAcel = 2.0f;
@@ -19,7 +21,6 @@ public class ProxyRobotControl : MonoBehaviour {
 	
 	public float chargeAcel = 10.0f;
 	public float maxChargeSpeed = 50.0f;
-	public float attackTime = 0.5f;
 
 	public float doubleSpaceTime = 0.4f;
 	public float attackRotSpeed = 15f;
@@ -45,6 +46,15 @@ public class ProxyRobotControl : MonoBehaviour {
 	float sideAnim;
 	float forthAnim;
 	public float planarAnimRate = 0.5f;
+	public int attackLine;
+	public float attackTime = 0.5f;
+	public float cooldownTime = 1.0f;
+	int attacksMax=0;
+	public int attacksGround = 4;
+	public int attacksAir = 3;
+	public bool attackFull = false;
+	public bool coolingDown;
+	public int lastAttack=1;
 
 	enum state {
 		free,
@@ -78,135 +88,196 @@ public class ProxyRobotControl : MonoBehaviour {
 		//without delay, player rotates as fast as the camera
 		//myTransform.forward = projOnFloor;
 		
-		switch (mystate) {
+		switch(mystate){
 			case state.free:
+				VerticalThrust();
+				Thrust();
+				DirectionalThrust();
 				Movement();
-				Listen4Attack();				
+				CheckGround();
+				
+				Listen4Attack();
+				if(attackLine>0)
+					StartCoroutine(simpleAttack());				
+			break;
+			case state.attacking:
+				VerticalThrust();
+				CalculateInertiaMovement();
+				Movement();
+				Listen4Attack();
 			break;
 		}
 	}
 
 	void Listen4Attack(){
-		if(Input.GetKeyDown(KeyCode.Mouse0)){
-			StartCoroutine(simpleAttack());
+		if (attackFull == false && coolingDown == false){			
+			if(Input.GetKeyDown(KeyCode.Mouse0)){
+				if (!attackFull) {
+					attackLine++;
+					if(attackLine >= attacksMax){
+						attackFull = true;
+						attackLine = attacksMax;
+					}
+				}
+			}
 		}
 	}
 	IEnumerator simpleAttack(){
-		myAnim.SetBool("takeOff", false);
-		myAnim.SetBool("attack", true);
-		lethal = true;
+		myAnim.SetInteger("attackID", lastAttack);
+		mystate = state.attacking;
+		myAnim.SetBool("attacking", true);
 
-		yield return new WaitForSeconds(attackTime);
-		myAnim.SetBool("attack", false);
+		yield return new WaitForSeconds(0.25f*attackTime);
+		lethal = true;
+		yield return new WaitForSeconds(0.75f*attackTime);
 		lethal = false;
-	}
-	void Movement(){
-		//vertical movement
-		if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)){
-			vertSpeed = Mathf.Lerp (vertSpeed, maxVertVelUp, vertAcel * Time.deltaTime);
-			myAnim.SetFloat("vertSpeed", vertSpeed/maxVertVelUp);
-			myAnim.SetBool("grounded", false);
+
+		//decide if follow a combo or returns to idle
+		attackLine--;
+
+		if (attackLine > 0 && lastAttack < attacksMax) {
+			lastAttack++;
+			StartCoroutine( simpleAttack() );
 		}
 		else{
+			StartCoroutine( RestartAttacks() );
+		}
+	}
+	IEnumerator RestartAttacks(){
+		coolingDown = true;
+		yield return new WaitForSeconds(cooldownTime);
+		coolingDown = false;
+		lastAttack = 1;
+		mystate = state.free;
+		attackFull = false;
+		myAnim.SetBool("attacking", false);
+	}
+
+	void VerticalThrust(){
+		//vertical movement
+		if ((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) && grounded){
+			startJumpTime = Time.time;
+			myAnim.SetBool ("grounded", false);
+		}			
+		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+			if(!grounded || Time.time - startJumpTime > jumpTime){
+				vertSpeed = Mathf.Lerp (vertSpeed, maxVertVelUp, 0.5f*vertAcel * Time.deltaTime);
+				myAnim.SetFloat ("vertSpeed", vertSpeed / maxVertVelUp);
+			}
+		} else {
 			//making sure the oscillation always start upward
-			if(Input.GetKeyDown(KeyCode.Space))
+			if (Input.GetKeyDown (KeyCode.Space))
 				timeOffset = Time.time;
-			if(Input.GetKey(KeyCode.Space)){				
+			if (Input.GetKey (KeyCode.Space)) {				
 				vertSpeed = Mathf.Lerp (vertSpeed, 0.0f, vertAcel * Time.deltaTime);
-				vertSpeed += oscilSpeed*Mathf.Sin(oscilFreq*(Time.time-timeOffset));
-				myAnim.SetFloat("vertSpeed", 0.0f);
+				vertSpeed += oscilSpeed * Mathf.Sin (oscilFreq * (Time.time - timeOffset));
+				myAnim.SetFloat ("vertSpeed", 0.0f);
+				attacksMax = attacksAir;
 				grounded = false;
-				myAnim.SetBool("grounded", false);
+				myAnim.SetBool ("grounded", grounded);
+			} else {
+				vertSpeed = Mathf.Lerp (vertSpeed, -maxVertVelDown, myGravity * Time.deltaTime);
+				myAnim.SetFloat ("vertSpeed", vertSpeed / maxVertVelDown);
+			}
+		}
+	}
+	void CheckGround(){
+		if(Time.time - startJumpTime > jumpTime){
+			if(myControl.isGrounded) {
+				groundCounter = 0;
+				grounded = true;
+				vertSpeed = 0.0f;
+				attacksMax = attacksGround;
+				//Debug.Log("<color=green>grounded</color>");
 			}
 			else{
-				vertSpeed = Mathf.Lerp (vertSpeed, -maxVertVelDown, myGravity * Time.deltaTime);
-				myAnim.SetFloat("vertSpeed", vertSpeed/maxVertVelDown);
-			}
-		}
-		
-		if (myControl.isGrounded){
-			groundCounter = 0;
-			grounded = true;
-			vertSpeed = 0.0f;
-			myAnim.SetBool("grounded", true);
-			//Debug.Log("<color=green>grounded</color>");
-		}
-		else{
-			groundCounter++;
-			if (groundCounter > groundLimit){
-				grounded = false;
-				myAnim.SetBool("grounded", false);
-				//Debug.Log("<color=red>not grounded</color>");
-			}
-		}
-		
-		//sideways movement
-		if(Input.GetKey(KeyCode.D)){
-			sideSpeed = maxVel*myTransform.right;
-			sideAnim = Mathf.Lerp(sideAnim, 1.0f, planarAnimRate*Time.deltaTime);
-		}
-		else{
-			if(Input.GetKey(KeyCode.A)){
-				sideSpeed = -maxVel*myTransform.right;
-				sideAnim = Mathf.Lerp(sideAnim, -1.0f,  planarAnimRate*Time.deltaTime);
-
-			}
-			else{				
-				sideSpeed = Vector3.zero;
-				sideAnim = Mathf.Lerp(sideAnim, 0.0f,  planarAnimRate*Time.deltaTime);
-			}
-		}
-		myAnim.SetFloat("side", sideAnim);
-
-		//forth movement
-		if(Input.GetKey(KeyCode.W)){
-			forthSpeed = maxVel*myTransform.forward;
-			forthAnim = Mathf.Lerp(forthAnim, 1.0f,  planarAnimRate*Time.deltaTime);
-		}
-		else{
-			if(Input.GetKey(KeyCode.S)){
-				forthSpeed = -maxVel*myTransform.forward;
-				forthAnim = Mathf.Lerp(forthAnim, -1.0f,  planarAnimRate*Time.deltaTime);
-			}
-			else{				
-				forthSpeed = Vector3.zero;
-				forthAnim = Mathf.Lerp(forthAnim, 0.0f,  planarAnimRate*Time.deltaTime);
-			}
-		}
-
-		//power chargem
-		if(Input.GetKey(KeyCode.Mouse1)){
-			chargeSpeed = Mathf.Lerp(chargeSpeed, maxChargeSpeed, chargeAcel*Time.deltaTime);
-			movement = chargeSpeed*camTransform.forward;		
-			vertSpeed = Vector3.Project(movement, Vector3.up).y;
-			planarMovement = Vector3.ProjectOnPlane(movement, Vector3.up);
-			forthAnim = Mathf.Lerp(forthAnim, 1.0f,  2*planarAnimRate*Time.deltaTime);
-		}
-		else{//normal move
-			chargeSpeed = 0.0f;
-			//move
-			thrustVec = sideSpeed + forthSpeed;
-			
-			
-			//if no input, deacelerate
-			if(thrustVec.magnitude == 0f){
-				//Debug.Log("<color=orange>Deacelerating: "+planarMovement.magnitude.ToString()+"</color>");
-				planarMovement = Vector3.Lerp(planarMovement, Vector3.zero, frictionAcel*Time.deltaTime);
-				
-				if(planarMovement.magnitude <= imperceptibleSpeed){
-					//Debug.Log("<color=orange>Deacelerated to zero!</color>");
-					planarMovement = Vector3.zero;
+				groundCounter++;
+				if (groundCounter > groundLimit) {
+					grounded = false;
+					attacksMax = attacksAir;
+					//Debug.Log("<color=red>not grounded</color>");
 				}
 			}
-			else{
-				//Debug.Log("<color=blue>thrustVec.magnitude: "+thrustVec.magnitude+"</color>");
-				planarMovement = Vector3.Lerp(planarMovement, thrustVec, inertiaFactor*Time.deltaTime);
+			myAnim.SetBool ("grounded", grounded);
+		}
+	}
+
+	void Thrust(){		
+		//sideways movement
+		if (Input.GetKey (KeyCode.D)) {
+			sideSpeed = maxVel * myTransform.right;
+			sideAnim = Mathf.Lerp (sideAnim, 1.0f, planarAnimRate * Time.deltaTime);
+		} else {
+			if (Input.GetKey (KeyCode.A)) {
+				sideSpeed = -maxVel * myTransform.right;
+				sideAnim = Mathf.Lerp (sideAnim, -1.0f, planarAnimRate * Time.deltaTime);
+
+			} else {				
+				sideSpeed = Vector3.zero;
+				sideAnim = Mathf.Lerp (sideAnim, 0.0f, planarAnimRate * Time.deltaTime);
 			}
 		}
+		myAnim.SetFloat ("side", sideAnim);
+
+		//forth movement
+		if (Input.GetKey (KeyCode.W)) {
+			forthSpeed = maxVel * myTransform.forward;
+			forthAnim = Mathf.Lerp (forthAnim, 1.0f, planarAnimRate * Time.deltaTime);
+		} else {
+			if (Input.GetKey (KeyCode.S)) {
+				forthSpeed = -maxVel * myTransform.forward;
+				forthAnim = Mathf.Lerp (forthAnim, -1.0f, planarAnimRate * Time.deltaTime);
+			} else {				
+				forthSpeed = Vector3.zero;
+				forthAnim = Mathf.Lerp (forthAnim, 0.0f, planarAnimRate * Time.deltaTime);
+			}
+		}
+
+	}
+	void DirectionalThrust(){
+		//power chargem
+		if(Input.GetKey(KeyCode.Mouse1)){
+			PowerThrust();
+		} else{
+			CalculateInertiaMovement();
+		}
+	}
+
+	void PowerThrust(){
+		chargeSpeed = Mathf.Lerp (chargeSpeed, maxChargeSpeed, chargeAcel * Time.deltaTime);
+		movement = chargeSpeed * camTransform.forward;		
+		vertSpeed = Vector3.Project (movement, Vector3.up).y;
+		planarMovement = Vector3.ProjectOnPlane (movement, Vector3.up);
+		forthAnim = Mathf.Lerp (forthAnim, 1.0f, 2 * planarAnimRate * Time.deltaTime);	
+	}
+	void CalculateInertiaMovement(){
+		//normal move
+		chargeSpeed = 0.0f;
+		//move
+		thrustVec = sideSpeed + forthSpeed;
+
+
+		//if no input, deacelerate
+		if (thrustVec.magnitude == 0f){
+			//Debug.Log("<color=orange>Deacelerating: "+planarMovement.magnitude.ToString()+"</color>");
+			planarMovement = Vector3.Lerp (planarMovement, Vector3.zero, frictionAcel * Time.deltaTime);
+
+			if (planarMovement.magnitude <= imperceptibleSpeed) {
+				//Debug.Log("<color=orange>Deacelerated to zero!</color>");
+				planarMovement = Vector3.zero;
+			}
+		} 
+		else{
+			//Debug.Log("<color=blue>thrustVec.magnitude: "+thrustVec.magnitude+"</color>");
+			planarMovement = Vector3.Lerp (planarMovement, thrustVec, inertiaFactor * Time.deltaTime);
+		}
+	}
+	void Movement(){
 		myAnim.SetFloat("forth", forthAnim);
 		movement = (vertSpeed*Vector3.up + planarMovement)*Time.deltaTime;
 		myControl.Move(movement);
 	}
+
 	void OnTriggerEnter(Collider other){
 		//if(other.tag!="Player")
 		//	Debug.Log("<color=black>"+gameObject.name + "triger enter with "+other.name+"</color>");
